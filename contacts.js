9279889e703e9428f665a7c1647173f7404facb0
MrZICS <dzobaoksana1993@gmail.com>
2023-04-11 12:59:24 +0200

    last

--- contacts.js
+++ contacts.js
@@ -1,43 +1,67 @@
 const fs = require("fs").promises;
-const path = require("path");
 const chalk = require("chalk");
-const {nanoid} = require("nanoid");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-async function listContacts(){
- const contacts = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(contacts)
-};
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-async function getContactById(contactId){
- 
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+
+  if (!contact) {
+    console.log(
+      chalk.red(
+        `Contact with this id ${contact} does not exist, please try again`
+      )
+    );
+    return;
+  }
+  return contact;
+}
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+
+  const index = contacts.findIndex((item) => item.id === contactId);
+  const deleteContact = contacts.splice(index, 1);
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    console.log(
+      chalk.green(
+        `This contact with ID ${contactId} has been successfully deleted`
+      )
+    );
+  } catch (error) {
+    console.log(error);
+  }
+  return deleteContact;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+  return result || null;
+}
+
+async function addContact(name, email, phone) {
+  try {
     const contacts = await listContacts();
-    const contact = contacts.find(contact => contact.id === contactId);
-    if (!contact){
-     console.log(chalk.bgYellow("Sorry, contact not found!"))
-     return };
-    console.log(chalk.green("Yes, contact found"), contact)
-    };
+    const newContact = { name, email, phone, id: uuidv4() };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
+    console.log(chalk.green(`Contact created: ${name}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-    async function removeContact(contactId){
-      const contacts = await listContacts();
-      const updatedContacts = contacts.filter(
-        (contact) => contact.id !== contactId
-      );
-  
-      if (contacts.length === updatedContacts.length) {
-        console.log(chalk.red("Sorry, there is no such contact!"));
-        return;
-      }
-  
-      await fs.writeFile(contactsPath, JSON.stringify(updatedContacts), "utf-8");
-      console.log(chalk.green("Contact is successfully deleted!"))
-    }
-    async function addContact(name, email, phone){
-      const contacts = await listContacts();
-      const newContact = { id: nanoid(), name, email, phone };
-      contacts.push(newContact);
-      await fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
-      console.log(chalk.green("Contact is successfully created:"), newContact);
-    }
+module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,79 +1,47 @@
-// const { Command } = require("commander");
-// const { listContacts, getContactById, addContact, removeContact } = require("./contacts");
-// const program = new Command();
-// program
-//   .option("-a, --action <type>", "choose action")
-//   .option("-i, --id <type>", "user id")
-//   .option("-n, --name <type>", "user name")
-//   .option("-e, --email <type>", "user email")
-//   .option("-p, --phone <type>", "user phone");
 
-// program.parse(process.argv);
-
-// const argv = program.opts();
-
-
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//         console.table(listContacts());
-//       break;
-
-//     case "get":
-//        getContactById(id);
-//       break;
-
-//     case "add":
-//       addContact(name, email, phone)
-//       break;
-
-//     case "remove":
-//        removeContact(id)
-//       break;
-
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(argv);
 const {
   listContacts,
   getContactById,
   removeContact,
   addContact,
 } = require("./contacts");
-
+const contacts = require("./contacts.js");
 const { Command } = require("commander");
+const chalk = require("chalk");
 const program = new Command();
 program
-.option("-a, --action <type>", "choose action")
-.option("-i, --id <type>", "user id")
-.option("-n, --name <type>", "user name")
-.option("-e, --email <type>", "user email")
-.option("-p, --phone <type>", "user phone");
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
-
-function invokeAction({ action, id, name, email, phone }) {
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-     listContacts();
+      console.table(await listContacts())
       break;
 
     case "get":
-    getContactById(id)
+      const contact = await contacts.getContactById(id);
+      if (!contact) {        
+        return console.log(chalk.bgRed(`Contact with id ${id} not found!`));
+      }
+      console.log(chalk.blue('%s'), contact);
       break;
+     
 
     case "add":
-     addContact(name, email, phone)
+      await addContact(name, email, phone)
       break;
 
     case "remove":
-  removeContact(id)
+      await removeContact(id)
       break;
 
     default:
